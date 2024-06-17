b6cec72ff55f66f82d73003ae0beca134e8bb233
Bernardo <bavaldivieso@uc.cl>
2024-06-17 15:04:33 -0400

    feat: create MenuOverlay component

---
+++ app/components/menu-overlay.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "./nav-link";
+
+export const MenuOverlay = ({
+  links,
+}: {
+  links: { path: string; title: string }[];
+}) => {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
